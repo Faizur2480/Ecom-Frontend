381d6e6ae41bc789f64509d806944b900985f6fa
FaizRahman <faizurrahman@promantus.com>
2022-11-23 16:25:39 +0530

    updates

--- src/Utilis/CommonPath.js
+++ src/Utilis/CommonPath.js
@@ -2,5 +2,5 @@
 // export const BASE_URL ="http://localhost:8080/api/"
 
 // Production Environment
-export const BASE_URL ="https://devopslearnapi.azurewebsites.net"
+export const BASE_URL ="https://devlearnapi.azurewebsites.net"
 
